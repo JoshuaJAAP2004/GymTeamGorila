6337dca999e015f76b1c34967456289b89072ec7
Joshua <Joshua@DESKTOP-08HII17>
2024-06-11 14:33:48 -0500

    Mi aporte es el "Bienvenido a mi repositorio git hub"

---
+++ src/Registro/Registro01.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Registro;
+
+/**
+ *
+ * @author Joshua
+ */
+public class Registro01 {
+    public static void main(String[] args) {
+        System.out.println("Hola bienvenido a mi repositorio");
+    }
+}
